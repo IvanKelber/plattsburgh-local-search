3c071886405b4fd57d931047ea3a32405540b624
Ivan Kelber <ivankelber@gmail.com>
2017-02-27 17:46:58 -0500

    added skeleton

---
+++ TSP/proposals.py
@@ -0,0 +1,49 @@
+import random
+
+def swapRandom(points):
+    proxy = points[:]
+    length = len(points)
+    i = random.randint(0,length-1)
+    j = random.randint(0,length-1)
+    proxy[i],proxy[j] = proxy[j],proxy[i]
+    return proxy
+
+def reverse(points):
+    return points[::-1]
+
+def swapPairs(points):
+    i = random.randrange(0,len(points)-1) #randrange doesn't include upper bound
+    j = random.randrange(0,len(points)-1) #randrange doesn't include upper bound
+    proxy = points[:]
+    proxy[i:i+1],proxy[j:j+1] = proxy[j:j+1], proxy[i:i+1]
+    return proxy
+
+
+def cutCards(points):
+    i = random.randint(0,len(points)-1)
+    proxy = points[i:] + points[:i]
+    return proxy
+
+#Doesn't seem to work that well
+def swapBeginning(points):
+    proxy = points[:]
+    length = len(points)
+    i = random.randint(1,length-1)
+    proxy[0],proxy[i] = proxy[i],proxy[0]
+    return proxy
+
+
+def shuffle(points):
+    proxy = points[:]
+    random.shuffle(proxy)
+    return proxy
+
+def swapLocal(points):
+    proxy = points[:]
+    i = random.randrange(0,len(points)-1) #randrange doesn't include upper bound
+    proxy[i],proxy[i+1] = proxy[i+1],proxy[i]
+    return proxy
+
+def shiftRight(points):
+    proxy = points[1:] + [points[0]]
+    return proxy
---
+++ TSP/tspRunner.py
@@ -0,0 +1,81 @@
+import random
+import math
+import sys
+import time
+
+sys.path.append('../') #add parent directory to import path
+from graphics import GraphWin,Point,Circle,Line,Rectangle
+from basic import localSearch
+
+from proposals import *
+
+
+
+
+PROPOSALS = [
+    swapPairs,
+    swapRandom,
+    # swapBeginning,
+    swapLocal,
+    # shuffle,
+]
+WINDOW_SIZE = 800
+MARGIN = WINDOW_SIZE/4
+NUM_POINTS = 50
+POINT_RADIUS = 3
+
+# POINTS = [(random.randrange(MARGIN,WINDOW_SIZE-MARGIN),random.randrange(MARGIN,WINDOW_SIZE-MARGIN)) for i in range(NUM_POINTS)]
+
+def tsp(points):
+    total = 0
+    for p,q in zip(points,points[1:]):
+        total += distance(p,q)
+    return total
+
+def distance(p,q):
+    return ((q[0]-p[0])**2 + (q[1]-p[1])**2)**.5
+
+def plot(win,points):
+    r = Rectangle(Point(0,0),Point(WINDOW_SIZE,WINDOW_SIZE))
+    r.setFill("white")
+    r.draw(win)
+    for p,q in zip(points,points[1:]):
+        Line(Point(p[0],p[1]),Point(q[0],q[1])).draw(win)
+        c = Circle(Point(p[0],p[1]),POINT_RADIUS)
+        c.setFill("red")
+        c.setOutline("red")
+        c.draw(win)
+
+def circle():
+    return [((WINDOW_SIZE/4)*(math.sin(i*math.pi*2/NUM_POINTS)+1)+MARGIN,(WINDOW_SIZE/4)*(math.cos(i*math.pi*2/NUM_POINTS)+1)+MARGIN) for i in range(NUM_POINTS)]
+
+
+def main():
+    POINTS = circle()
+    random.shuffle(POINTS)
+
+    win = GraphWin("My Circle", WINDOW_SIZE, WINDOW_SIZE)
+
+
+    epsilons = [1000,100,500,200,200,50,100]
+    xp = POINTS
+    bestSoFar = xp
+    print "Starting Guess: %.10f" % tsp(POINTS)
+    start = time.clock()
+    for eps in epsilons:
+        for i in range(5):
+            plot(win,xp)
+            xp = localSearch(tsp,random.choice(PROPOSALS),xp,eps,3)
+            if(tsp(xp) <= tsp(bestSoFar)):
+                bestSoFar = xp
+        print "Best for epsilon %.2f: %.10f" %(eps, tsp(bestSoFar))
+    print "=========="
+    print "Original: ", tsp(POINTS)
+    print "Best: ", tsp(bestSoFar)
+    end = time.clock()
+    plot(win,bestSoFar)
+
+    raw_input("Took %.4f seconds. Press enter to close:" % (end-start))
+
+if __name__ == '__main__':
+    main()
